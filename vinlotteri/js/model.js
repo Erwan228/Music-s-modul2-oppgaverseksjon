f37a78de108c15b3de9f02254d2edbaa1df42478
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-01-27 12:41:51 +0100

    laget modell

---
+++ vinlotteri/js/model.js
@@ -0,0 +1,11 @@
+model = {
+    state: "navnListe", /*navnListe eller vinnere*/
+    input: {
+        inputName: null,
+        numberOfWinners: 0,
+    },
+    data:{
+        employees,
+    },
+    drawnEmployees: [],
+}
